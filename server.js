6c4cb334dc79b2b0c685f1d26131fc6297bbb180
soRodriguezz <s.rodriguezzapata01@gmail.com>
2022-05-20 23:57:49 -0400

    mongodb connect

--- server.js
+++ server.js
@@ -1,7 +1,17 @@
 const express = require('express');
+const morgan = require('morgan');
+const cors = require('cors');
+const connectDb = require('./database');
+
+require('dotenv').config();
 
 const app = express();
 
-const port = process.env.PORT || 5000;
+connectDb();
 
+
+app.use(morgan('dev'));
+app.use(cors());
+
+const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Server started on port ${port}`));
