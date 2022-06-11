dbd83bd81f4afa21fe12b6e6650d176100c14c99
soRodriguezz <s.rodriguezzapata01@gmail.com>
2022-06-10 21:24:25 -0400

    se crean roles

--- server.js
+++ server.js
@@ -1,17 +1,31 @@
 const express = require('express');
+const connectDb = require('./database');
 const morgan = require('morgan');
 const cors = require('cors');
-const connectDb = require('./database');
+const { readdirSync } = require('fs');
+const swaggerUI = require('swagger-ui-express');
+const swaggerJsdoc = require('swagger-jsdoc');
+const swaggerConfig = require('./documentation/swagger.config.json');
+
+const {createRoles,createUserAdmin} = require('./helpers/initialApp');
 
 require('dotenv').config();
 
 const app = express();
 
+createRoles()
+    .then(() => createUserAdmin())
+    .catch(error => console.error(error));
+
 connectDb();
 
-
 app.use(morgan('dev'));
+app.use(express.json({limit: '2mb'}));
+const swaggerDocs = swaggerJsdoc(swaggerConfig);
+app.use("/api/docs", swaggerUI.serve, swaggerUI.setup(swaggerDocs, { explorer: true }));
 app.use(cors());
+
+readdirSync('./routes').map((r) => app.use('/api', require(`./routes/${r}`)));
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Server started on port ${port}`));
